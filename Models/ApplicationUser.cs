bf23b436b61116ff0eb15e1f06d74d85d68cf92f
Anastasia Stefanescu <119175350+anastasia-stefanescu@users.noreply.github.com>
2024-01-09 15:50:52 +0200

    Adaugare info extra user pt profil

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -4,5 +4,10 @@ namespace Proiect.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string? NumeComplet { get; set; }
+
+        public string? Rol { get; set; }
+
+        public byte[]? PozaProfil { get; set; }
     }
 }
